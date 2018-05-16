c77380b2d1c747f695fe16d012ab5cd41cc11b8a
kallepakarinen <cuttu85@gmail.com>
2018-05-16 12:13:06 +0300

    starting project, add contact, contact-list, service

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {ContactListComponent} from './contact/contact-list/contact-list.component';
+import {ContactService} from './contact/contact.service';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    ContactService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contact/contact.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Contact} from './contact';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  contacts: Contact[];
+
+
+  constructor() {
+    this.contacts = [];
+    this.contacts.push(new Contact('Kalle', 'Pakarinen'));
+    this.contacts.push(new Contact('Joku', 'Toinen'));
+    this.contacts.push(new Contact('Vielä', 'Kolmas'));
+  }
+
+  getContacts(): Contact[] {
+    return this.contacts;
+  }
+}
