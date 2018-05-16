18cdf7344f494c5564b2e7028c73aaee98cdaec8
kallepakarinen <cuttu85@gmail.com>
2018-05-16 12:16:13 +0300

    added contact id property, create services folder

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Contact} from './contact';
+import {Contact} from '../contact';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +10,9 @@ export class ContactService {
 
   constructor() {
     this.contacts = [];
-    this.contacts.push(new Contact('Kalle', 'Pakarinen'));
-    this.contacts.push(new Contact('Joku', 'Toinen'));
-    this.contacts.push(new Contact('Vielä', 'Kolmas'));
+    this.contacts.push(new Contact(1,'Kalle', 'Pakarinen'));
+    this.contacts.push(new Contact(2,'Joku', 'Toinen'));
+    this.contacts.push(new Contact(3,'Vielä', 'Kolmas'));
   }
 
   getContacts(): Contact[] {
